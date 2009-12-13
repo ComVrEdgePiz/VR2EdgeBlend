527065866cb41c72045da5dde873da787b5f6416
Your Name <inf6718@fh-wedel.de>
2009-12-13 18:18:16 +0100

    inital

---
+++ blend.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   blend.h
+ * Author: flatline
+ *
+ * Created on December 13, 2009, 2:23 PM
+ */
+
+#ifndef _BLEND_H
+#define	_BLEND_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _BLEND_H */
+
---
+++ edge.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   edge.h
+ * Author: flatline
+ *
+ * Created on December 13, 2009, 2:23 PM
+ */
+
+#ifndef _EDGE_H
+#define	_EDGE_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* _EDGE_H */
+
